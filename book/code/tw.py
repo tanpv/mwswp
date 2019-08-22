91793a4bc5920d916fe710dcfa6d578708337542
tanpham <tanpham@emotiv.com>
2019-08-22 14:00:58 +0700

    update

--- book/code/tw.py
+++ book/code/tw.py
@@ -5,11 +5,7 @@ import tweepy
 from  tweepy import  StreamListener
 from  tweepy import  Stream
 
-# for tw
-consumer_key = 'ehkQnDRIQsKf9y8hzGqIZVkQt'
-consumer_secret = 'xnUbEqzv9yC10l6py5DESC7MCOeuWfQhIRbobdxqF4jiBoHllV'
-access_key = '312132184-ogXwvGCrHh73By67WeE95Vf6lPN7boFqoLeGhBnX'
-access_secret = 'vV0wx1epPBPqh7he0M85yq2Awrt2Qm47s7NeBo7oINxnO'
+
 
 # OAuth process, using the keys and tokens
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
